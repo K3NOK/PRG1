8ab45d67236c83d9dff00688d321fd2d169ad1e0
K3N0K . <filonnewman@gmail.com>
2025-10-02 19:05:19 +0200

    Initial commit

---
+++ 1_Zadania 1_10_2025/Zadanie_2.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,e,c,d,f,x,y,D,Dx,Dy;
+    
+    cout<< "podaj wspolczynniki pierwszego równania" << endl;
+    cin >> a >> b >> e;
+    
+    cout<< "podaj wspolczynniki drugiego równania" << endl;
+    cin >> c >> d >> f;
+    
+    D = a * d - c * b;
+    
+    if (D == 0) {cout << "układ jest nieoznaczony lub sprzeczny" << endl;
+    }
+    
+    else{
+        
+        Dx = e * d - f * b;
+        Dy = a * f - e * c;
+        x = Dx / D;
+        y = Dy / D;
+        
+        cout<< "rozwiazaniem układu są liczby "<< "x = " << x << " y = " << y << endl;
+    }
+    
+    
+    return 0;
+}
+
+
