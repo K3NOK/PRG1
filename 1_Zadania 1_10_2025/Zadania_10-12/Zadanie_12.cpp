b3a05b0e3e22f3a0aa06a5b4a07a11b6fd6524ed
K3NOK <filonnewman@gmail.com>
2025-10-25 12:34:57 +0200

    Zad 12 ver.2

--- 1_Zadania 1_10_2025/Zadania_10-12/Zadanie_12.cpp
+++ 1_Zadania 1_10_2025/Zadania_10-12/Zadanie_12.cpp
@@ -3,45 +3,75 @@
 using namespace std;
 
 int main(){
-    int p,q,wygrana;
+    int p,q,wygrana,ver;
     char proceed;
     proceed = 't';
-    //srand(time(NULL));
+    cout << "wybierz wersję z której chciał byś skorzystać" <<endl<< "jeśli chcesz skorzystać z wersji z losowaniem wpisz: 1"<<endl<< "jeśli chcesz manualnie wprowadzić losy wpisz: 2"<<endl<<"wersja: ";
+    cin >> ver;
+    srand(time(NULL));
     //rand() % 6 +1;
-    while(proceed == 't'||proceed == 'T'){
-        cout << endl << "podaj zmienną p"<< endl;
-        cin >> p;
-        cout <<"podaj zmienną q"<< endl;
-        cin >> q;
-        
-        if (p % 2 == 0){
-            if (q == 2 || q == 4 || q == 5){
-                wygrana = p + (3 * q);
-            } else {
-                wygrana = 2 * q;
-            }
-        }else{
-            if (q == 2|| q == 4 || q == 5){
-                wygrana = min(p,q) + 4;
-            }else{
-                if (p == q){
-                    wygrana = (5 * p) + 3;
+
+    if (ver == 2){
+        while(proceed == 't'||proceed == 'T'){
+                cout << endl << "podaj zmienną p"<< endl;
+                cin >> p;
+                cout <<"podaj zmienną q"<< endl;
+                cin >> q;
+                
+                if (p % 2 == 0){
+                    if (q == 2 || q == 4 || q == 5){
+                        wygrana = p + (3 * q);
+                    } else {
+                        wygrana = 2 * q;
+                    }
                 }else{
-                    wygrana = (2 * q) + p;
+                    if (q == 2|| q == 4 || q == 5){
+                        wygrana = min(p,q) + 4;
+                    }else{
+                        if (p == q){
+                            wygrana = (5 * p) + 3;
+                        }else{
+                            wygrana = (2 * q) + p;
+                        }
+                    }
                 }
+                if (p == 5, q == 5){
+                    wygrana += 5;
+                }
+                cout << "q= " << q << ", p = " << p << " a wygrana wynosi " << wygrana << endl;
+                cout << "czy chcesz kontynuować? t/n:";
+                cin >> proceed;
             }
-        }
-        if (p == 5, q == 5){
-            wygrana += 5;
-        }
-        cout << "q= " << q << ", p = " << p << " a wygrana wynosi " << wygrana << endl;
-        cout << "czy chcesz kontynuować? t/n:";
-        cin >> proceed;
-        
-
-
+    } else if (ver == 1){
+        while(proceed == 't'||proceed == 'T'){
+                p = rand() % 6 +1;
+                q = rand() % 6 +1;
+                if (p % 2 == 0){
+                    if (q == 2 || q == 4 || q == 5){
+                        wygrana = p + (3 * q);
+                    } else {
+                        wygrana = 2 * q;
+                    }
+                }else{
+                    if (q == 2|| q == 4 || q == 5){
+                        wygrana = min(p,q) + 4;
+                    }else{
+                        if (p == q){
+                            wygrana = (5 * p) + 3;
+                        }else{
+                            wygrana = (2 * q) + p;
+                        }
+                    }
+                }
+                if (p == 5, q == 5){
+                    wygrana += 5;
+                }
+                cout << "q= " << q << ", p = " << p << " a wygrana wynosi " << wygrana << endl;
+                cout << "czy chcesz kontynuować? t/n:";
+                cin >> proceed;
+            }
     }
-
+    
 
     return 0;
 }
