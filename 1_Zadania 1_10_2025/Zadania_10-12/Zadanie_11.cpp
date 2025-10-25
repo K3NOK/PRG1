77347150500268f57add6695d3060a1f6b33befa
K3NOK <filonnewman@gmail.com>
2025-10-25 08:28:09 +0200

    Zad_11

---
+++ 1_Zadania 1_10_2025/Zadania_10-12/Zadanie_11.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double PLNperH, nadgodziny, przegodziny,brutto, netto;
+    string katzasz;
+
+    while (katzasz != "STOP") {
+
+        cout << "podaj kategorię zaszergowania pracownika (A/B/C/D) lub wpisz STOP aby wyłączyć program" << endl;
+        cin >> katzasz;
+
+        if (katzasz == "STOP") {
+            break;
+        } else if (katzasz == "A") {
+            PLNperH = 15;
+        } else if (katzasz == "B") {
+            PLNperH = 25;
+        } else if (katzasz == "C") {
+            PLNperH = 30;
+        } else if (katzasz == "D") {
+            PLNperH = 35;
+        }
+
+
+        cout << "podaj liczbę przepracowanych godzin w tygodniu" << endl;
+        cin >> przegodziny;
+
+        if (przegodziny > 40.0) {
+            nadgodziny = przegodziny - 40;
+            brutto = (40 * PLNperH) + (nadgodziny * PLNperH * 2);
+        } else {
+            brutto = przegodziny * PLNperH;
+        }
+
+        if (brutto <= 700){
+            netto = brutto * 0.85;
+        } else if (brutto > 700 && brutto <= 1200) {
+            netto = brutto * 0.8;
+        } else {
+            netto = brutto * 0.75;
+        }
+        
+        cout << " - - - Pracownik zarobił przez tydzien " << netto << "PLN netto lub " << brutto << "PLN brutto. - - - " << endl<<endl;
+    }
+    return 0;
+}
+
+
+//brutto - na brudno całość bez podatkow
+//netto - chajs po odjęciu podatkow
