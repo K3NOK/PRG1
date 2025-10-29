800c81aa311326d22e41128262dd10749447f3cd
K3NOK <filonnewman@gmail.com>
2025-10-29 16:07:24 +0100

    zadania 13-15(1)

---
+++ 1_Zadania 1_10_2025/Zadania_13-15/Zadanie_13.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+double n ,suma ,p ,p1 ;
+cout << "podaj n" << endl;
+cin >> n;
+
+suma = 0
+p = sqrt(n)
+
+while (p > 1) {
+    if ((n % p) = 0){
+        suma += p
+        p1 = (n / p)
+        if (p1 != p){
+            suma += p1
+            }
+        }
+        p -= 1
+    }
+suma += 1
+if (suma = n){
+    cout << n << "JLD" << endl;
+}else{
+    cout << n << "NJLD"<< endl;
+}
+
+
+
+
+
+
+    return 0;
+}
