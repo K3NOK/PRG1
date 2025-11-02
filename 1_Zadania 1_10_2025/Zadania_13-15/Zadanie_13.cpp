e653ef424f051b71d1ab317aa228ad3556db2599
K3NOK <filonnewman@gmail.com>
2025-11-02 16:56:57 +0100

    zrobione zadanie 13

--- 1_Zadania 1_10_2025/Zadania_13-15/Zadanie_13.cpp
+++ 1_Zadania 1_10_2025/Zadania_13-15/Zadanie_13.cpp
@@ -3,34 +3,34 @@
 using namespace std;
 
 int main(){
-double n ,suma ,p ,p1 ;
-cout << "podaj n" << endl;
-cin >> n;
+int n ,suma ,p ,p1 ,status;
+status = 1;
 
-suma = 0
-p = sqrt(n)
+while (status != 0){
+    cout << "podaj n" << endl;
+    cin >> n;
+    suma = 0;
+    p = sqrt(n);
 
-while (p > 1) {
-    if ((n % p) = 0){
-        suma += p
-        p1 = (n / p)
-        if (p1 != p){
-            suma += p1
+    while (p > 1) {
+        if (n % p == 0) {
+            suma += p;
+            p1 = (n / p);
+            if (p1 != p) {
+                suma += p1;
+                }
             }
+            p -= 1;
         }
-        p -= 1
+    suma += 1;
+    if (suma == n){
+        cout << n << " JLD" << endl;
+    }else{
+        cout << n << " NJLD"<< endl;
     }
-suma += 1
-if (suma = n){
-    cout << n << "JLD" << endl;
-}else{
-    cout << n << "NJLD"<< endl;
+
+    cout << "czy chcesz kontynuowac? " << endl << "0 - nie , 1 - tak" << endl;
+    cin >> status;
 }
-
-
-
-
-
-
     return 0;
 }
