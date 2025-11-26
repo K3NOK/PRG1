a1ff9b393e96cd36f343c2d1f394ea591cef8721
K3NOK <filonnewman@gmail.com>
2025-11-26 17:03:37 +0100

    kollos zad3 done

--- 1_Zadania 1_10_2025/KOLOKWIUM/s35482z3.cpp
+++ 1_Zadania 1_10_2025/KOLOKWIUM/s35482z3.cpp
@@ -1,1 +1,34 @@
-  
+#include<iostream>
+using namespace std;
+
+int main(){
+string a;
+int i,ilejedynek,tmplicz,dlugosc;
+cout << "prosze podac ciag 0,1: ";
+cin >>a;
+a+="01";
+//ilejedynek = 0;
+int tablica[dlugosc];
+tmplicz=0;
+dlugosc = 0;
+for(i =0;i<a.length();i++){
+    if (a[i] == '1'){
+        //ilejedynek+=1;
+        tmplicz+=1;
+    }else if (a[i] =='0'){
+        if (tmplicz>0){
+            tablica[dlugosc]=tmplicz;
+            dlugosc+=1;
+            tmplicz=0;
+        }
+    }
+}
+for (i = 0;i<dlugosc;i++){
+    cout<<tablica[i];
+}
+
+
+    cout<<endl;
+    system("pause");
+    return 0;
+}
