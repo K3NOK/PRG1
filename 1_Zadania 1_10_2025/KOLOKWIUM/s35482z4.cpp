3905480183ac90a535c9c3cbabeccb3529e224c1
K3NOK <filonnewman@gmail.com>
2025-11-26 17:57:00 +0100

    prawie skonczone piate

--- 1_Zadania 1_10_2025/KOLOKWIUM/s35482z4.cpp
+++ 1_Zadania 1_10_2025/KOLOKWIUM/s35482z4.cpp
@@ -1,1 +1,12 @@
- 
+ #include<iostream>
+using namespace std;
+
+int main(){
+string k,p;
+k="";
+p="p";
+cout<<k.length()<<k;
+k+=p;
+cout<<k.length()<<k;
+return 0;
+}
--- 1_Zadania 1_10_2025/KOLOKWIUM/s35482z5.cpp
+++ 1_Zadania 1_10_2025/KOLOKWIUM/s35482z5.cpp
@@ -1,1 +1,42 @@
- 
+#include<iostream>
+using namespace std;
+
+int main(){
+int n,i,tmp,m,pierczynie;
+string tmmp,max;
+cout<<"podaj liczbe elementow w tablicy: ";
+cin>>n;
+int tablica[n];
+tmp=0;
+pierczynie =0;
+tmmp="";
+max="";
+for (i=0;i<n;i++){
+    cout <<"podaj "<< i+1 << " element: ";
+    cin>> tablica[i];
+}
+for (i=0;i<n;i++){
+    if (tablica[i]>=tablica[i-1]){
+        tmp+=1;
+        if (pierczynie = 0){
+            tmmp+=tablica[i-1];
+        }
+        tmmp+=tablica[i];
+        pierczynie+=1;
+        if (tmmp.length()>max.length()){
+            max = tmmp;
+        }
+    }else{
+        tmp=0;
+        tmmp="";
+    }
+    //cout<<tmp<<endl;
+}
+
+
+//cout<<tmmp;
+cout<<max<<endl;
+
+system("pause");
+return 0;
+}
