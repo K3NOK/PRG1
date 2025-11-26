4ff02b87d7a464cdeeb4de5734d4936f5471aaa2
K3NOK <filonnewman@gmail.com>
2025-11-26 16:15:44 +0100

    zad2 kolo done

--- 1_Zadania 1_10_2025/KOLOKWIUM/s35482z2.cpp
+++ 1_Zadania 1_10_2025/KOLOKWIUM/s35482z2.cpp
@@ -1,1 +1,29 @@
- 
+#include<iostream>
+using namespace std;
+
+int main(){
+int n,i,s;
+cout<<"podaj liczbe elementow w tablicy: ";
+cin>>n;
+int tablica[n];
+for (i=0;i<n;i++){
+    cout <<"podaj "<< i+1 << " element: ";
+    cin>> tablica[i];
+}
+s=tablica[0];
+for (i=0;i<n;i++){ 
+    if(s < tablica[i]){
+        s=tablica[i];
+    }else if (s > tablica[i]){
+        cout<<"false"<<endl;
+        system("pause");
+        return false;
+    }
+}
+
+
+
+    cout<<"true"<<endl;
+    system("pause");
+    return true;
+}
