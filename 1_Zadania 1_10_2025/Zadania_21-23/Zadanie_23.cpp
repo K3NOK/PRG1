6852d559f0a67bdd4ad21e8b4b658835b50cbf6e
K3NOK <filonnewman@gmail.com>
2025-11-30 13:51:29 +0100

    zrobione zadabue 23

--- 1_Zadania 1_10_2025/Zadania_21-23/Zadanie_23.cpp
+++ 1_Zadania 1_10_2025/Zadania_21-23/Zadanie_23.cpp
@@ -3,12 +3,46 @@
 using namespace std;
 
 int main(){
+    int a,b,c,d;
+    int tablica1[100][100];
+    int tablica2[100][100];
+    int tblica_z_wynikiem[100][100];
+    cout<<"prosze kolejno podac wartosci a, b, c, d. po kazdej potwiedzic naciskajac enter."<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    if(a<0||b<0||c<0||d<0||a!=c||c!=d){
+        cout<<"BLAD";
+        return 0;
+    }
+//wczytywanie wartosci podanych macierzy po wierszach (chyba)
+    for (int i=0;i<a;i++){
+        for (int j=0;j<b;j++){
+            cout<<"podaj "<< j+1<< " wartosc z "<<i+1<<" rzedu macierzy axb"<<endl;
+            cin>>tablica1[i][j];
+        }
+    }
+    for (int i=0;i<c;i++){
+        for (int j=0;j<d;j++){
+            cout<<"podaj "<< j+1<< " wartosc z "<<i+1<<" rzedu macierzy cxd"<<endl;
+            cin>>tablica2[i][j];
+        }
+    }
+//-------dodawanie i wynik
+    cout<<endl<<"wynik to:"<<endl;
+    for (int i=0;i<a;i++){
+        for (int j=0;j<b;j++){
+            tblica_z_wynikiem[i][j] = tablica1[i][j]+tablica2[i][j];
+            cout<< tblica_z_wynikiem[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 
 
 
 
-
-    system("pause");
+    //system("pause");
     return 0;
 }
---
+++ 1_Zadania 1_10_2025/testy.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b;
+cout<< "nigga";
+cin >> a>>b;
+cout<< a<<" ts " <<b;
+
+
+
+    return 0;
+}
