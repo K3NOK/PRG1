a98f863ea9c94f3e2bc749c01b443fe5f8112b96
K3NOK <filonnewman@gmail.com>
2025-12-17 09:39:40 +0100

    Zad 28 done

--- 1_Zadania 1_10_2025/Zadania_27-29/Zadanie_28.cpp
+++ 1_Zadania 1_10_2025/Zadania_27-29/Zadanie_28.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
+
+int* no_clue(){
+    int* ptr = new int;
+    return ptr;
+}
+
+
+
 int main(){
-    
-
-
-
+    int* call = no_clue();
+    delete call;
     return 0;
 }
