7f2d3ce247e91f17f51852698a4d359dc535fc0c
K3NOK <filonnewman@gmail.com>
2026-01-06 16:12:43 +0100

    done ex 29th

--- 1_Zadania 1_10_2025/Zadania_27-29/Zadanie_29.cpp
+++ 1_Zadania 1_10_2025/Zadania_27-29/Zadanie_29.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int mian(){
-    int a,b;
-    cout << "podaj pierwszy arg: ";
-    cin >> a;
-    cout << "podaj drugi arg: ";
-    cin >> b;
-    int* ptr1 = &a;
-    int* ptr2 = &b;
 
+void funkcja(int* ptr1,int* ptr2){
     if (*ptr2 < *ptr1){
         int tmp = *ptr1;
         *ptr1 = *ptr2;
         *ptr2 = tmp;
     }
+}
+
+int main(){
+    int a,b;
+    cout << "podaj pierwszy arg: ";
+    cin >> a;
+    cout << "podaj drugi arg: ";
+    cin >> b;
+
+    //int* ptr1 = &a;
+    //int* ptr2 = &b;
+
+    //if (*ptr2 < *ptr1){
+    //    int tmp = *ptr1;
+    //    *ptr1 = *ptr2;
+    //    *ptr2 = tmp;
+    //}
+
+    funkcja(&a,&b);
 
     cout << "pierwszy ptr wskazuje na wartosc: " << a << endl<<"drugi ptr wskazuje na wartosc: " << b;
     return 0;
